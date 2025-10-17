b801a1548aa257e8343de82b6752ef9d9bce7154
Md Ahosanul Islam <ahosanul81@gmail.com>
2025-10-17 15:11:29 +0600

    add linkedlist----->>> singlyLinkedList--->> pop method

--- linked-list/singlyLinkedList.js
+++ linked-list/singlyLinkedList.js
@@ -21,6 +21,32 @@ class singlyLinkedList {
     }
     this.length++;
   }
+  pop() {
+    if (!this.head) {
+      return null;
+    }
+    if (this.length === 1) {
+      const removedNode = this.head;
+      this.head = null;
+      this.tail = null;
+      this.length = 0;
+      return removedNode;
+    }
+    let currentNode = this.head;
+    let lastNode = this.tail;
+    let newLastNode;
+    while (currentNode) {
+      if (currentNode.next === this.tail) {
+        newLastNode = currentNode;
+        break;
+      }
+      currentNode = currentNode.next;
+    }
+    newLastNode.next = null;
+    this.tail = newLastNode;
+    this.length--;
+    return lastNode;
+  }
 }
 
 const list = new singlyLinkedList();
@@ -30,4 +56,5 @@ list.push(5);
 list.push(10);
 list.push(14);
 list.push(16);
-console.log(list.head.next.next.next);
+list.pop();
+console.log(list);
