45263ce77440014c82e18cce13a58511441abc93
Md Ahosanul Islam <ahosanul81@gmail.com>
2025-10-20 19:30:08 +0600

    added  linkedlist -> stack with linked list ->push(),  pop()

---
+++ linked-list/stackWithLinkedList.js
@@ -0,0 +1,55 @@
+class Node {
+  constructor(val) {
+    this.value = val;
+    this.next = null;
+  }
+}
+
+class BookStack {
+  constructor() {
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
+  push(val) {
+    const book = new Node(val);
+    if (!this.top) {
+      this.top = book;
+      this.bottom = book;
+      return this;
+    } else {
+      //   let currentNode = this.top;
+      //   while (currentNode.next === null) {
+      //     if (currentNode.next === null) {
+      //       currentNode.next = book;
+      //       console.log(currentNode.next);
+      //       this.length++;
+      //       this.bottom = book;
+      //       break;
+      //     }
+      //     currentNode = currentNode.next;
+      //   }
+      let holdingBook = this.top;
+      this.top = book;
+      this.top.next = holdingBook;
+    }
+    this.length++;
+    return this;
+  }
+  pop() {
+    if (!this.top) {
+      return null;
+    }
+    this.top = this.top.next;
+    this.length--;
+    return this;
+  }
+}
+const list = new BookStack();
+list.push("book1");
+list.push("book2");
+list.push("book3");
+list.push("book4");
+list.pop();
+
+console.log(list);
